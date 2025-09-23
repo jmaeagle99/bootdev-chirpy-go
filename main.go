33687c7dc604db56a456ce44cd5fd0466da281cf
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-22 20:00:56 -0700

    Add file server for root path

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
