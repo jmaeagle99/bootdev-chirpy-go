5484ace153d44c842c7258b89fcffce548746a5e
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-27 12:15:02 -0700

    Add SQL migration

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "pgcrypto"; -- enables gen_random_uuid()
+
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMPTZ DEFAULT now(),
+    email TEXT NOT NULL UNIQUE
+);
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS trigger AS $$
+BEGIN
+  NEW.updated_at := now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+-- +goose StatementEnd
+
+CREATE TRIGGER users_set_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
+
+-- +goose Down
+DROP TRIGGER IF EXISTS users_set_updated_at ON users;
+DROP FUNCTION IF EXISTS set_updated_at();
+DROP TABLE IF EXISTS users;
