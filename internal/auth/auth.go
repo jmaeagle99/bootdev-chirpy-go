1bde21dc53aae31e54a2f52d2fbc3cd8e062343f
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-28 19:50:18 -0700

    Add token generation and validation

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"encoding/base64"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -8,4 +15,46 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func decodeTokenSecret(tokenSecret string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(tokenSecret)
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	decodedTokenSecret, err := decodeTokenSecret(tokenSecret)
+	if err != nil {
+		return "", err
+	}
+
+	now := time.Now().UTC()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	return token.SignedString(decodedTokenSecret)
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+
+	_, err := jwt.ParseWithClaims(
+		tokenString,
+		&claims,
+		func(token *jwt.Token) (interface{}, error) {
+			return decodeTokenSecret(tokenSecret)
+		})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	subject, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return subject, nil
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,115 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestCheckPasswordHash(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword456!"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
+
+	tests := []struct {
+		name          string
+		password      string
+		hash          string
+		wantErr       bool
+		matchPassword bool
+	}{
+		{
+			name:          "Correct password",
+			password:      password1,
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: true,
+		},
+		{
+			name:          "Incorrect password",
+			password:      "wrongPassword",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Password doesn't match different hash",
+			password:      password1,
+			hash:          hash2,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Empty password",
+			password:      "",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Invalid hash",
+			password:      password1,
+			hash:          "invalidhash",
+			wantErr:       true,
+			matchPassword: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && match != tt.matchPassword {
+				t.Errorf("CheckPasswordHash() expects %v, got %v", tt.matchPassword, match)
+			}
+		})
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	defaultUserId := uuid.MustParse("b92b8014-6b95-42df-99d6-1e78551bb6ea")
+	defaultTokenSecret := "SGVsbG8sIFdvcmxkIQ=="
+	defaultExpiry, _ := time.ParseDuration("5s")
+	jwt1, _ := MakeJWT(defaultUserId, defaultTokenSecret, defaultExpiry)
+	jwt2, _ := MakeJWT(defaultUserId, "VW5leHBlY3RlZCB0b2tlbiBzZWNyZXQ=", defaultExpiry)
+
+	tests := []struct {
+		name           string
+		tokenString    string
+		decodeSecret   string
+		expectedError  bool
+		expectedUserId uuid.UUID
+	}{
+		{
+			name:           "Valid token with expected subject",
+			tokenString:    jwt1,
+			decodeSecret:   defaultTokenSecret,
+			expectedError:  false,
+			expectedUserId: defaultUserId,
+		},
+		{
+			name:           "Mismatched signature",
+			tokenString:    jwt2,
+			decodeSecret:   defaultTokenSecret,
+			expectedError:  true,
+			expectedUserId: uuid.Nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actualUserId, err := ValidateJWT(tt.tokenString, tt.decodeSecret)
+			if (err != nil) != tt.expectedError {
+				t.Errorf("ValidateJWT() error = %v, expectedError %v", err, tt.expectedError)
+			}
+			if actualUserId != tt.expectedUserId {
+				t.Errorf("ValidateJWT() expects %v, got %v", tt.expectedUserId, actualUserId)
+			}
+		})
+	}
+}
